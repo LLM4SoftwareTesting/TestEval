40f6dc1da205aee3f9b5570f5be56da7d4d164e6
Wang Wenhan <wwhjacob@hotmail.com>
2024-10-06 21:47:16 -0600

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -22,3 +22,31 @@ def add_lineno(code):
     for i, line in enumerate(lines):
         new_code+=f'{i+1}. {line}\n'
     return new_code
+
+
+def add_lineno_comment(code,docstring_lines=None):
+    """Add line numbers to code as comments."""
+    lines=code.split('\n')
+    for i in range(len(lines)-1,-1,-1):
+        if lines[i]=='':
+            lines.pop(i)
+        else:
+            break
+    new_code=''
+    if docstring_lines is None:
+        for i, line in enumerate(lines):
+            if i == len(lines) - 1:
+                new_code+=f'{line}  #{i+1}'
+            else:
+                new_code+=f'{line}  #{i+1}\n'
+    else:
+        docstart,docend=docstring_lines
+        for i, line in enumerate(lines):
+            if i>=docstart and i<=docend:
+                new_code+=f'{line}\n'
+            else:
+                if i == len(lines) - 1:
+                    new_code+=f'{line}  #{i+1}'
+                else:
+                    new_code+=f'{line}  #{i+1}\n'
+    return new_code
